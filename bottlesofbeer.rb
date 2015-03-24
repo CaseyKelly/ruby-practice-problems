0722b649dc25f900f7feb9d7140b53a062001d5d
CaseyKelly <casey.kelly@colorado.edu>
2015-03-24 11:02:17 -0600

    bottles of beer works

--- bottlesofbeer.rb
+++ bottlesofbeer.rb
@@ -1,1 +1,22 @@
+beer = 99
+while beer != 0 && beer != 1 && beer != 2
+  puts beer.to_s + ' bottles of beer on the wall. ' + beer.to_s + ' bottles of beeeeer.'
+  beer = beer - 1
+  puts 'Take one down, pass it around, ' + beer.to_s + ' bottles of beer on the wall.'
+end
 
+if beer == 2
+  puts beer.to_s + ' bottles of beer on the wall. ' + beer.to_s + ' bottles of beeeeer.'
+  beer = beer - 1
+  puts 'Take one down, pass it around, ' + beer.to_s + ' bottle of beer on the wall.'
+end
+
+if beer == 1
+  puts beer.to_s + ' bottle of beer on the wall. ' + beer.to_s + ' bottle of beeeeer.'
+  beer = beer - 1
+  puts 'Take one down, pass it around, ' + beer.to_s + ' bottles of beer on the wall.'
+end
+
+if beer == 0
+  puts 'Go home, you\'re drunk.'
+end
